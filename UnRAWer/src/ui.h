b6aa2a41e5018ca27b60dcf21f5cce1e80fb2118
Vlad (Kuzmin) Erium <libalias@gmail.com>
2024-04-17 21:20:34 +0900

    version bump

--- UnRAWer/src/ui.h
+++ UnRAWer/src/ui.h
@@ -39,7 +39,7 @@
 #include "process.h"
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 47
+#define VERSION_MINOR 48
 
 void setPBarColor(QProgressBar* progressBar, const QColor& color = QColor("#05B8CC"));
 
